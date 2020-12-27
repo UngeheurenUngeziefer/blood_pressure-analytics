5856101df45086a1a27616d85d4150630bc005ef
jleaniuk <jauhien_leaniuk@gmail.com>
2020-12-27 20:37:57 +0300

    first

---
+++ pressure.py
@@ -0,0 +1,11 @@
+import numpy as np
+import pandas as pd
+
+pressure = pd.read_excel('Pressure.xlsx', sheet_name='Pressure')
+
+# print(pressure.dtypes)
+
+# pressure = pressure.index
+# number_of_rows = len(pressure)
+# print(number_of_rows)
+
