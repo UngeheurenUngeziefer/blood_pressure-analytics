c025073afeb6a33d2521ed5f735bf4a0555710ba
jleaniuk <jauhien_leaniuk@gmail.com>
2020-12-31 15:26:34 +0300

    remove folder

--- pressure.py
+++ pressure.py
@@ -1,7 +1,8 @@
 import numpy as np
 import pandas as pd
-from datetime import date
+from datetime import date, timedelta, datetime
 import pygal
+pd.options.mode.chained_assignment = None
 
 
 class Pressure:
@@ -11,20 +12,35 @@ class Pressure:
 		# open given excel table
 		self.df = pd.read_excel(excel, sheet_name)
 		#converting to correct datatypes
-
-	def print_df(self):
-		# print table
-		print(self.df)
-
-	def null_time_value_filling(self):
-
-		
-	def converting(self):
-		# convert cells of excel to understandable datatypes
 		self.df.Date = pd.to_datetime(self.df.Date,
 		 									format='%d.%m.%Y')
 		self.df.Time = pd.to_datetime(self.df.Time, 
 											format='%H:%M:%S').dt.time
+		# average time of measurements
+		self.df['Date_freq'] = self.df.groupby('Date')['Date'].transform('count')
+
+		# for every index of column
+		for i in range(len(self.df.Time)):
+			# if Time is Null and previous not Null
+			# if Date is same and there 4 or less dates
+			if pd.isnull(self.df.Time[i]) == True \
+			   and pd.isnull(self.df.Time[i - 1]) == False \
+			   and self.df.Date[i] == self.df.Date[i - 1] \
+			   and self.df.Date_freq[i] <= 4:
+				self.df.Time[i] = self.df.Time[i - 1]
+			else:
+				print('full')
+		
+
+	def print_df(self):
+		# print table
+		# print(self.df.head(50))
+		print(self.df.Date_freq.unique())
+
+		
+	def converting(self):
+		# convert cells of excel to understandable datatypes
+		
 		
 		self.df.Upper = self.df.Upper.astype(int)
 		# self.df.DateTime = str(self.df.Date) + ' ' + str(self.df.Time)
@@ -39,5 +55,5 @@ class Pressure:
 # excel file need to have columns in order:
 # Date, Time, Upper, Down, Pulse, Comments (optional)
 obj = Pressure('Pressure.xlsx', 'Pressure')
-obj.converting()
+obj.print_df()
 
