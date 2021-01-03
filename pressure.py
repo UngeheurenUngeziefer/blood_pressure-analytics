e624d466167f56affe3940d640137de7d8f08b55
jleaniuk <jauhien_leaniuk@gmail.com>
2021-01-03 18:00:09 +0300

    analysis completed

--- pressure.py
+++ pressure.py
@@ -9,6 +9,9 @@ class Pressure:
 	''' class aggregating analytics of blood pressure '''
 
 	def __init__(self, excel, sheet_name):
+		''' initializing class, open excel, convert datatypes,
+			filling empty time records '''
+
 		# open given excel table
 		self.df = pd.read_excel(excel, sheet_name)
 
@@ -16,11 +19,11 @@ class Pressure:
 		self.df.Date = pd.to_datetime(self.df.Date,	format='%d.%m.%Y')
 		self.df.Time = pd.to_datetime(self.df.Time, format='%H:%M:%S').dt.time
 		
-		# frequency of measurements
+		# creating frequency of measurements column
 		self.df['Date_freq'] = \
 							self.df.groupby('Date')['Date'].transform('count')
 
-		# for every index of column
+		# filling the empty time records if we know time of near records
 		for i in range(len(self.df.Time)):
 			
 			# if Time is Null and previous not Null and Date the same
@@ -52,8 +55,9 @@ class Pressure:
 			else:
 				pass
 
+
 	def minus_2_hours(self, index):
-		'''function extract 2 hours and return timestamp'''
+		'''function extract 2 hours from previous time and return timestamp'''
 		delta = timedelta(hours = 2)
 		current_date = date(self.df.Date[index].year, \
 							self.df.Date[index].month, \
@@ -76,14 +80,8 @@ class Pressure:
 		print(self.df)
 
 
-	def measurements_analytics(self):
-		average_measurements = sum(self.df.Date_freq) / len(self.df.Date_freq)
-		maximum_measurements = max(self.df.Date_freq)
-		minimum_measurements = min(self.df.Date_freq)
-		sum_measurements	 = sum(self.df.Date_freq)
-
-
-	def dates_analytics(self):
+	def new_dates_columns(self):
+		'''creating columns related to dates'''
 		self.df['Measurements_per_month'] = \
 			self.df.Date.groupby(
 			[self.df.Date.dt.year, self.df.Date.dt.month]).transform('count')
@@ -95,6 +93,7 @@ class Pressure:
 		list_of_unique_series = self.df.Date.unique()
 		self.df['Unique_dates'] = \
 			pd.DataFrame(list_of_unique_series)
+		pd.to_datetime(self.df.Unique_dates, format='%d.%m.%Y')
 
 		self.df['Days_of_observations_per_month'] = \
 			self.df.Unique_dates.groupby(
@@ -105,27 +104,114 @@ class Pressure:
 			self.df.Unique_dates.groupby(
 				[self.df.Unique_dates.dt.year]).transform('count')
 
-	def time_analytics(self):
-		for i in self.df.Time:
-			datetime.strptime('Jun 1 2005  1:33PM', '%b %d %Y %I:%M%p')
-			if i >= datetime.strptime('5:00:00', '%H:%M:%S').time() \
-			and i < datetime.strptime('11:00:00', '%H:%M:%S').time():
-				print('Morning')
-		# self.df['Times_of_day'] = \
-		# same with season
+
+	def new_times_columns(self):
+		'''creating columns related to time'''
+		self.df['Times_of_day_4'] = self.df['Time']
+		self.df['Times_of_day_2'] = self.df['Time']
+		self.df['Season'] = self.df['Date']
+
+		# creating time 2 types
+		for i in range(len(self.df['Times_of_day_2'])):
+			if self.df['Times_of_day_2'].iloc[i] >= datetime.strptime('5:00:00', '%H:%M:%S').time() \
+			and self.df['Times_of_day_2'].iloc[i] < datetime.strptime('17:00:00', '%H:%M:%S').time():
+				self.df['Times_of_day_2'].iloc[i] = 'Day'
+			elif self.df['Times_of_day_2'].iloc[i] >= datetime.strptime('17:00:00', '%H:%M:%S').time() \
+			and self.df['Times_of_day_4'].iloc[i] <= datetime.strptime('23:59:59', '%H:%M:%S').time() \
+			or self.df['Times_of_day_2'].iloc[i] < datetime.strptime('5:00:00', '%H:%M:%S').time():
+				self.df['Times_of_day_2'].iloc[i] = 'Night'
+			else:
+				self.df.Time.iloc[i] = ''
+
+		# creating time 4 types
+		for i in range(len(self.df['Times_of_day_4'])):
+			if self.df['Times_of_day_4'].iloc[i] >= datetime.strptime('5:00:00', '%H:%M:%S').time() \
+			and self.df['Times_of_day_4'].iloc[i] < datetime.strptime('11:00:00', '%H:%M:%S').time():
+				self.df['Times_of_day_4'].iloc[i] = 'Morning'
+			elif self.df['Times_of_day_4'].iloc[i] >= datetime.strptime('11:00:00', '%H:%M:%S').time() \
+			and self.df['Times_of_day_4'].iloc[i] < datetime.strptime('17:00:00', '%H:%M:%S').time():
+				self.df['Times_of_day_4'].iloc[i] = 'Day'
+			elif self.df['Times_of_day_4'].iloc[i] >= datetime.strptime('17:00:00', '%H:%M:%S').time() \
+			and self.df['Times_of_day_4'].iloc[i] < datetime.strptime('23:00:00', '%H:%M:%S').time():
+				self.df['Times_of_day_4'].iloc[i] = 'Evening'
+			elif self.df['Times_of_day_4'].iloc[i] >= datetime.strptime('23:00:00', '%H:%M:%S').time() \
+			and self.df['Times_of_day_4'].iloc[i] <= datetime.strptime('23:59:59', '%H:%M:%S').time() \
+			or self.df['Times_of_day_4'].iloc[i] < datetime.strptime('5:00:00', '%H:%M:%S').time():
+				self.df['Times_of_day_4'].iloc[i] = 'Night'
+			else:
+				self.df['Times_of_day_4'].iloc[i] = ''
+
+		# creating season column
+		for i in range(len(self.df['Season'])):
+			if self.df['Season'].iloc[i].month in (12, 1, 2):
+				self.df['Season'].iloc[i] = 'Winter'
+			elif self.df['Season'].iloc[i].month in (3, 4, 5):
+				self.df['Season'].iloc[i] = 'Spring'
+			elif self.df['Season'].iloc[i].month in (6, 7, 8):
+				self.df['Season'].iloc[i] = 'Summer'
+			elif self.df['Season'].iloc[i].month in (9, 10, 11):
+				self.df['Season'].iloc[i] = 'Autumn'
+			else:
+				self.df['Season'].iloc[i] = ''
 
 
 	def print_info(self):
-		print('Avg number of measurements is ' + str(average_measurements)[0:4])
-		print('Min number of measurements is ' + str(minimum_measurements))
-		print('Max number of measurements is ' + str(maximum_measurements))
-		print('Total number of measuremets is ' + str(sum_measurements)) 
-			
+		''' print basic info '''
+		
+		average_measurements = sum(self.df.Date_freq) / len(self.df.Date_freq)
+		maximum_measurements = max(self.df.Date_freq)
+		minimum_measurements = min(self.df.Date_freq)
+		total_measurements	 = len(self.df.Date)
+		
+		self.df.at[0, 'Avg number of measurements per day'] = str(average_measurements)[0:4]
+		self.df.at[0, 'Min number of measurements per day'] = str(minimum_measurements)
+		self.df.at[0, 'Max number of measurements per day'] = str(maximum_measurements)
+		self.df.at[0, 'Total number of measuremets'] = str(total_measurements)
+		self.df.at[0, 'Avg number of systolic pressure'] = self.df.Upper.mean()
+		self.df.at[0, 'Min number of systolic pressure'] = min(self.df.Upper)
+		self.df.at[0, 'Max number of systolic pressure'] = max(self.df.Upper)
+		self.df.at[0, 'Avg number of diastolic pressure'] = self.df.Down.mean()
+		self.df.at[0, 'Min number of diastolic pressure'] = min(self.df.Down)
+		self.df.at[0, 'Max number of diastolic pressure'] = max(self.df.Down)
+		self.df.at[0, 'Avg number of pulse'] = self.df.Pulse.mean()
+		self.df.at[0, 'Min number of pulse'] = min(self.df.Pulse)
+		self.df.at[0, 'Max number of pulse'] = max(self.df.Pulse)
 
-excel = Pressure('res/Pressure.xlsx', 'Pressure')
+		# print('Total measurements per year: ')
+		# print(self.df.groupby([self.df.Date.dt.year]
+		# 					 )['Measurements_per_year'].count().to_string())
+
+		# print('Total measurements per month: ')
+		# print(self.df.groupby([self.df.Date.dt.year,
+		# 	 				   self.df.Date.dt.month]
+		# 					 )['Measurements_per_month'].count().to_string())
+		# print('Total measurements per day: ')
+		# print(self.df.groupby([self.df.Date.dt.year,
+		# 				 	   self.df.Date.dt.month, 
+		# 				 	   self.df.Date.dt.day]
+		# 					 )['Date_freq'].count().to_string())
+		# print('Days of measurements per year: ')
+		# print(self.df.groupby([self.df.Unique_dates.dt.year.map('{:.0f}'.format)]
+		# 				 	 )['Unique_dates'].count().to_string())
+		# print('Days of measurements per month: ')
+		# print(self.df.groupby([self.df.Unique_dates.dt.year.map('{:.0f}'.format),
+		# 				 	   self.df.Unique_dates.dt.month.map('{:.0f}'.format)]
+		# 				 	 )['Unique_dates'].count().to_string())
+		# print('Times of day of measurements format-4: ')
+		# print(self.df.groupby(self.df.Times_of_day_4)['Date'].count().to_string())
+		# print('Times of day of measurements format-2: ')
+		# print(self.df.groupby(self.df.Times_of_day_2)['Date'].count().to_string())
+		# print('Season of measurements: ')
+		# print(self.df.groupby(self.df.Season)['Date'].count().rename({'Season':'asd'}).to_string())
+
+
+
+
+
 #excel.print_df()
+excel = Pressure('res/Pressure.xlsx', 'Pressure')
 
-# excel.measurements_analytics()
-# excel.dates_analytics()
-# excel.save_df('C:/Users/sewer/MyPython/Blood Pressure Analytics/Output Table.csv')
-excel.time_analytics()
+excel.new_dates_columns()
+excel.new_times_columns()
+excel.print_info()
+#excel.save_df('C:/Users/sewer/MyPython/Blood Pressure Analytics/Output Table.csv')
